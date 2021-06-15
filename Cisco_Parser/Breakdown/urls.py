4160dcaa79e035fcfb161956485a6bbb51f4b5e9
rapavin <rapavin@gmail.com>
2021-06-14 22:02:11 -0500

    Add files via upload

--- Cisco_Parser/Breakdown/urls.py
+++ Cisco_Parser/Breakdown/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.Home_Page, name='Home_Page'),
-    path('Firmware_Page',views.Firmware_Page, name='Firmware_Page'),
-    path('',views.stacking_Page, name='stacking_Page'),
+    path('Parser_Page',views.Parser_Page, name='Parser_Page'),
+    path('upload_read',views.upload_read, name='upload_read'),
 ]
--- Cisco_Parser/Breakdown/views.py
+++ Cisco_Parser/Breakdown/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.files.storage import FileSystemStorage
+from Interface_Cisco_Configuration_Parser import interface_ciscoconfparse
+from pathlib import Path
+import requests
 
 def Home_Page(request):
     return render(request, 'base.html')
 
-def Firmware_Page(request):
-    return render(request, 'firmware_page.html')
+def Parser_Page(request):
+    return render(request, 'Parser_Page.html')
 
-def stacking_Page(request):
-    return render(request, 'stacking_page.html')
+def file_move():
+    Path("/Users/praveenselvarajan/Desktop/Cisco_Parser/Cisco_Parser/interface_testing.csv").rename("/Users/praveenselvarajan/Desktop/Cisco_Parser/Cisco_Parser/media/interface_testing.csv")
+
+def upload_read(request):
+    running_configuration_list = []
+    f = request.FILES['running_config']
+    uploaded_file_readlines_list = request.FILES['running_config'].readlines()
+
+    for each_uploaded_file_readlines in uploaded_file_readlines_list:
+        running_configuration_list += [each_uploaded_file_readlines.decode().strip("\n").strip("\r")]
+
+    final_config = interface_ciscoconfparse.main(running_configuration_list)
+    file_move()
+
+    #http://127.0.0.1:8000/media/interface_testing.csv
+
+    return render(request, 'Parser_Page.html')
