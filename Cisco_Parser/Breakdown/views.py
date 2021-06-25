da52d68dd3d9a3bec61798cc56b2c5814603e20e
rapavin <rapavin@gmail.com>
2021-06-24 23:20:19 -0500

    Update views.py

--- Cisco_Parser/Breakdown/views.py
+++ Cisco_Parser/Breakdown/views.py
@@ -40,6 +40,8 @@ def upload_read(request):
         running_configuration_list_read = convert_each_uploaded_file_readlines_to_string()
         services_templates = cisco_conf_parser.import_textfsm_template(running_configuration_list_read)
         cisco_conf_parser.convert_services_result_csv()
+        f = open("services_config.csv", "r")
+        f.read()
         print(os.listdir())
         with open(os.path.join(BASE_DIR,'TEMP_FILE_STORAGE/interface_testing.csv'), 'rb') as fq:
             data_bytes = fq.read() 
