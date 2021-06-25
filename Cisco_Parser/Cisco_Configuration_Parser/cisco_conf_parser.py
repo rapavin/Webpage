2afd2bfc6db142c9dc259c470d3027f1ab444e05
rapavin <rapavin@gmail.com>
2021-06-24 22:59:10 -0500

    Update cisco_conf_parser.py

--- Cisco_Parser/Cisco_Configuration_Parser/cisco_conf_parser.py
+++ Cisco_Parser/Cisco_Configuration_Parser/cisco_conf_parser.py
@@ -22,8 +22,7 @@ def import_textfsm_template(reading_running_conf_read_string):
 				pass	
 
 def convert_services_result_csv():
-	with open('services.csv', 'w') as services_csv_file:
-		print(os.listdir())
+	with open('services_config.csv', 'w') as services_csv_file:
 		writer = csv.writer(services_csv_file)
 		writer.writerow(["HOSTNAME",switch_data['cisco_show_run_hostname'][0]])
 		writer.writerow(["SPANNING TREE MODE",switch_data['cisco_show_run_spanning_tree_mode'][0]])
@@ -63,7 +62,7 @@ def convert_services_result_csv():
 				writer.writerow(["SERVICES",each_switch_data[0]])
 			else:
 				writer.writerow(["SERVICES",each_switch_data[0]])
-		
+		return os.listdir()
 		
 def convert_l2_vlan_result_csv():
 	with open('l2_vlans_'+switch_data['cisco_show_run_hostname'][0]+'.csv', 'w') as l2_vlan_csv_file:
