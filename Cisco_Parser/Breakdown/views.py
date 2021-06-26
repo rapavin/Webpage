0bf7ad42936bd1c3c17bec431b1dfaf45f73546e
rapavin <rapavin@gmail.com>
2021-06-26 14:14:43 -0500

    Update views.py

--- Cisco_Parser/Breakdown/views.py
+++ Cisco_Parser/Breakdown/views.py
@@ -32,6 +32,21 @@ def convert_each_uploaded_file_readlines_to_string():
 
 def create_excel_file():
     workbook = xlsxwriter.Workbook('hello.xlsx')
+
+    # The workbook object is then used to add new 
+    # worksheet via the add_worksheet() method.
+    worksheet = workbook.add_worksheet()
+
+    # Use the worksheet object to write
+    # data via the write() method.
+    worksheet.write('A1', 'Hello..')
+    worksheet.write('B1', 'Geeks')
+    worksheet.write('C1', 'For')
+    worksheet.write('D1', 'Geeks')
+
+    # Finally, close the Excel file
+    # via the close() method.
+    workbook.close()
     
 def upload_read(request):
     create_excel_file()
