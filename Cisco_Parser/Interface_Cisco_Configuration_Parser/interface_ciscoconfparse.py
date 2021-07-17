7e7f3743d85c9ea645658e31c9dbcd231ee78ea9
rapavin <rapavin@gmail.com>
2021-07-17 13:56:32 -0500

    Update interface_ciscoconfparse.py

--- Cisco_Parser/Interface_Cisco_Configuration_Parser/interface_ciscoconfparse.py
+++ Cisco_Parser/Interface_Cisco_Configuration_Parser/interface_ciscoconfparse.py
@@ -169,11 +169,14 @@ def main(file_name):
         for key, items in empty_dic.items():
             writer.writerow([key,items[0],items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8],items[9],items[10],items[11],items[12],items[13],items[14],items[15],items[16],items[17],items[18],items[19],items[20],items[21],items[22],items[23],items[24]])
     
-    #print(os.getcwd())
-    with open("/home/ec2-user/webpage/Cisco_Parser/TEMP_FILE_STORAGE/interface_testing.csv", "r") as source:
-        reader = csv.reader(source)
-    print(reader)
-    print("working")
+    #
+    os.chdir(r"/home/ec2-user/webpage/Cisco_Parser/TEMP_FILE_STORAGE")
+    print(os.getcwd())
+    os.listdir(os.getcwd())
+    #with open("/home/ec2-user/webpage/Cisco_Parser/TEMP_FILE_STORAGE/interface_testing.csv", "r") as source:
+        #reader = csv.reader(source)
+    #print(reader)
+    #print("working")
     #print(reading_csv_pandas.head())
     
 if __name__ == "__main__":
